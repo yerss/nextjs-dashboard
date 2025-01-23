76cb81b32cae617c33072dbef639732209372220
yersinabdirazak <abdirazakyersin@gmail.com>
2025-01-23 19:40:07 +0500

    fix: font import

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: ['700', '400'] });
+export const lusitana = Lusitana({ weight: ['700', '400'], subsets: ['latin'] });
