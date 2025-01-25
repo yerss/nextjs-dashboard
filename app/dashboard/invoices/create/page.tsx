e9cbbc724990050521b7b6cc6c00e852e3be8514
yersinabdirazak <abdirazakyersin@gmail.com>
2025-01-25 16:01:13 +0500

    fix: invoices

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,22 @@
+import Form from '@/app/ui/invoices/create-form'
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+
+export default async function Page() {
+    const customers = await fetchCustomers();
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoice', href: '/dashboard/invoices' },
+                    {
+                        label: 'Create Invoice',
+                        href: '/dashboard/invoices.crate',
+                        active: true
+                    },
+                ]}
+            />
+            <Form customers={customers} />
+        </main>
+    )
+}
